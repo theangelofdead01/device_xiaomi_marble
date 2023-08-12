f5c78847e9e87d0dc2de4bc03e9024cd9446fa34
theangelofdead01 <rockinthecity142@gmail.com>
2023-08-11 21:34:06 -0500

    marble: SkylineUI bring-up

--- aosp_marble.mk
+++ aosp_marble.mk
@@ -8,17 +8,17 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common SkylineUI stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Inherit from marble device.
 $(call inherit-product, device/xiaomi/marble/device.mk)
 
 ## Device identifier
 PRODUCT_DEVICE := marble
-PRODUCT_NAME := lineage_marble
+PRODUCT_NAME := aosp_marble
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_MODEL := Redmi Note 12 Turbo
+PRODUCT_MODEL := POCO F5
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="marble"
@@ -26,12 +26,13 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 # GMS
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-# Rising OS Stuff with GApps
+# SkylineUI Stuff with GApps
+TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
 TARGET_USE_PIXEL_FINGERPRINT := true
 TARGET_ENABLE_BLUR := true
-WITH_GMS := true
 
-# Maintainer Stuff
-RISING_MAINTAINER := theangelofdead01
+# SkylineUI Maintainer Flags
+SKYLINEUI_MAINTAINER := theangelofdead01
+CUSTOM_BUILD_TYPE := OFFICIAL
